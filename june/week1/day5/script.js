a882e277cf84f22d0e0f61b3b6451876b05c1d2a
pragatiricms <pragati.nahak8658@gmail.com>
2025-06-07 10:34:56 +0530

    Add all the files of day5 folder

---
+++ june/week1/day5/script.js
@@ -0,0 +1,232 @@
+// console.log("Welcome to Qlith Innovation")
+
+// let a=1
+// let A=1
+// console.log(a)
+// console.log(A)
+
+// let _="Qlith";
+// console.log(_)
+
+// let $="Qlith Innvation";
+// console.log($)
+
+// let $name="Pragati";
+// console.log($name)
+
+// let _name="Pragati";
+// console.log(_name)
+
+// let nameOfStudent="Eleena";
+// console.log(nameOfStudent)
+
+// let EmployeeName="Eleena";
+// console.log(EmployeeName)
+
+// let a=20;
+// console.log(a)
+// console.log(typeof a);
+
+// let name1="Qlith";
+// console.log(name1);
+// console.log(typeof name1);
+
+// let isPass=true;
+// console.log(isPass)
+// console.log(typeof isPass)
+
+// let empty=undefined;
+// console.log(empty);
+// console.log(typeof empty)
+
+// let nullValue=null;
+// console.log(nullValue)
+// console.log(typeof nullValue)
+
+// let b;
+// console.log(b)
+
+// let c=30;
+// console.log(c)
+// console.log(typeof typeof c)
+
+// let d=30.5;
+// console.log(d)
+// console.log(typeof d)
+
+// //global declaration of the variable
+// var fullName="Qlith Innovation";
+// console.log(fullName);
+
+// {
+//     console.log(fullName)
+// }
+
+// function getName()
+// {
+//     console.log(fullName);
+// }
+
+// getName();
+
+// //normal block
+// //This is my block level declation of variable
+// {
+//     var a=40;
+//     console.log("block level declaration of variable",a)
+// }
+
+// console.log("local access",a)
+
+// function getA(){
+//     console.log("function leveldeclaration",a)
+// }
+// getA();
+
+// function variableDeclare(){
+//     var r=50;
+//     console.log(r)
+// }
+// variableDeclare();
+
+// console.log(r);
+
+//*this is the let variable declaration
+// let name="Innovation"
+// console.log(name)
+
+// {
+//     console.log(name)
+// }
+
+// function getName(){
+//     console.log(name)
+// }
+
+// getName();
+
+// //!block level declaration
+// {
+//     let a=30;
+//     console.log(a)
+// }
+
+// console.log("local access :",a)
+
+// function getA(){
+//     console.log("function level declaration :",a)
+// }
+
+// getA();
+
+//! This is the const variable declaration
+// const name="xyz";
+// console.log(name)
+// {
+//     console.log(name)
+// }
+
+// function getName(){
+//     console.log(name)
+// }
+// // getName();
+
+// {
+//     const a=70;
+//     console.log(a);
+// }
+
+// console.log(a);
+
+// var a=60;
+// console.log(a)
+
+// a=40;
+// console.log(a)
+
+// a="y";
+// console.log(a)
+
+// var a=30;
+// console.log(a)
+
+// let a=50;
+// console.log(a)
+
+// a=80;
+// console.log(a);
+
+// const s="string";
+// console.log(s);
+// s="rohan";
+// console.log(s);
+// const s="werth";
+// console.log(first)
+
+//~ Difference between var, let and const
+
+//redeclaration:
+// var=> possible
+// let=>not possible
+// const=>not possible
+
+//Reinitialization:
+// var=>possible
+// let=>possible
+// const=>not possible
+
+//Only declaration possible or not:
+// var=>possible
+// let=>possible
+// const=>not possible
+
+//Way of declartion and access:
+// global  declaration:
+//var
+// local access: access
+// block access: access
+// function access: access
+
+// let 
+//local access: access
+// block access: access
+// function access: access
+
+//const
+//local access: access
+// block access: access
+// function access: access
+
+// Block level declaration:
+//var
+// local access: possible
+// block access: access
+// function access:access
+
+//let
+//local access: not possible
+// block access: access
+// function access: not access
+
+// const
+//local access: not access
+// block access: not access
+// function access: access
+
+//Function level declaration:
+//var
+//local access: not possible
+// block access: not possible
+// function access: access
+
+//let
+//local access: not access
+// block access: not access
+// function access: access
+
+// const
+//local access: not access
+// block access: not access
+// function access: access
+
+
