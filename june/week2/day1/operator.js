4100685229d1da15ca00b47debf0913cc6348dd8
pragatiricms <pragati.nahak8658@gmail.com>
2025-06-10 11:31:46 +0530

    Add all the files and folders of week2 folder

---
+++ june/week2/day1/operator.js
@@ -0,0 +1,66 @@
+//operator
+//Arithmatic operator
+
+let a=5;
+let b=30
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%2)
+console.log(a**2)
+
+console.log(a++)
+console.log(++a)
+
+console.log(a)
+
+console.log(b--)
+console.log(--b)
+
+console.log("string"+30)
+console.log(20+30+"String")
+console.log(true+"String")
+console.log(+"30")
+console.log(+"text")
+console.log(+"30"+1)
+console.log(typeof +"text")
+console.log(+true)
+console.log(+true+3)
+console.log("20"+"30")
+console.log("20"+ +"30"*2)
+//console.log(++"20")error
+
+
+//Assignment operator
+let c=40;
+c+=2;
+console.log(c)
+c-=3;
+console.log(c)
+
+
+//Comparison operator
+let str1="xyz";
+let str2="y";
+console.log(str1==str2)
+console.log(str1===str2)
+
+//Normal comparison
+let n1="20"
+let n2="20"
+console.log(n1==n2)
+
+//strict comparison
+let N1="20"
+let N2=20
+console.log(N1==N2)//Normal comparison
+console.log(N1===N2)
+
+
+console.log(2>9)
+
+//Logical operator
+console.log((2<4) && (3<5))
+console.log((2>4) && (3<5))
+
